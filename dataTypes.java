e60da842efac3b573f98aba618f5dd4b477e20ab
Abhinav Gontu <45039240+AbhinavGontu@users.noreply.github.com>
2026-06-27 16:27:11 +0530

    data types

---
+++ dataTypes.java
@@ -0,0 +1,21 @@
+class dataTypes{
+
+    public static void main(String[] args){
+        byte b = 35;
+        short s = 32000;
+        int i = 150000;
+        long l = 3550000000L;
+        float f = 4.2f;
+        double d = 5.9;
+        boolean bool = true;
+        char c = '%';
+        System.out.printf("%d %d %d %d %.2f %f %b %c\n",b,s,i,l,f,d,bool,c);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(bool);
+        System.out.println(c);
+    }
+}
